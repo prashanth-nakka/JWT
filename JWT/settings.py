b3fd31f4c271d845b869ce806f7e53b3ba2fa863
Tooni <prashanth04@gmail.com>
2022-08-30 03:59:19 +0530

    Configured Email and added gitignore file

--- JWT/settings.py
+++ JWT/settings.py
@@ -142,6 +142,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
 
+# Email Configration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+
 
 # JWT Settings
 SIMPLE_JWT = {
